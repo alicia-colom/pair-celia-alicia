8a5dea3d1f9090dcf4e3cae309e9bf5bacdd5d82
alicia-colom <aliciacolom@gmail.com>
2020-11-03 21:29:37 +0100

    Add exercise 1 of lesson 3

---
+++ Module-3-lesson-3-exercise-1/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Card from '../components/Card';
+
+
+
+// EJERCICIO 1: 
+//
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Card/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Module-3-lesson-3-exercise-1/src/components/Card.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Card.scss';
+
+class Card extends React.Component {
+  render() {
+    const appHeader = (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-title">
+          <h1>Alex Guerrero</h1>
+          <h4>Lunes 02 de noviembre de 2020</h4>
+        </div>
+      </header>
+    );
+
+    const appMain = (
+      <main className="App-main">
+        <p className="App-text">
+          Lorem fistrum jarl a peich no puedor no puedor no te digo trigo por no
+          llamarte Rodrigor no te digo trigo por no llamarte Rodrigor ese
+          hombree qué dise usteer sexuarl. Mamaar está la cosa muy malar diodeno
+          por la gloria de mi madre diodeno me cago en tus muelas qué dise
+          usteer por la gloria de mi madre fistro se calle ustée va usté muy
+          cargadoo.
+        </p>
+      </main>
+    );
+
+    const appFooter = (
+      <footer className="App-bottom">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Leer más...
+        </a>
+        <small className="App-small">
+          <p>37</p>
+          <i className="fa fa-heart App-icon"></i>
+        </small>
+      </footer>
+    );
+
+    const appRoot = (
+      <div className="App">
+        {appHeader}
+        {appMain}
+        {appFooter}
+      </div>
+    );
+
+    return appRoot;
+  }
+}
+
+export default Card;
