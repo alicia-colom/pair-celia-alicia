77b2a8003ef8d41ed5c9df1d2e43a36f555d529a
alicia-colom <aliciacolom@gmail.com>
2020-11-05 01:10:36 +0100

    Add npm prop-types

--- Module-3-lesson-3-exercise-1+2+3+5+6/src/components/MediaList.js
+++ Module-3-lesson-3-exercise-1+2+3+5+6/src/components/MediaList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import Card from './Card';
 
 // EJERCICIO 5:
@@ -8,39 +8,43 @@ class MediaList extends Component {
 	render() {
 		return (
 			<section className="App">
-				<h1>A lot of Cards</h1>
+				<h1>A pair of Cards</h1>
 				<ul>
-					<li id="1">1
-						<Card />
+					<li id={1}>
+						<Card
+							name="Alicia Colom"
+							date="Martes 13"
+							text="Lorem fistrum jarl a peich no puedor no puedor no te digo trigo por no llamarte Rodrigor no te digo trigo por no llamarte Rodrigor ese hombree qué dise usteer sexuarl."
+							likes={1.000}
+						/>
 					</li>
-					{/* <li id="2">
-						<Card />
+					<li id={2}>
+						<Card
+							name="Pepi Flores"
+							date="Black Friday"
+							text="Mamaar está la cosa muy malar diodeno por la gloria de mi madre diodeno me cago en tus muelas qué dise usteer por la gloria de mi madre fistro se calle ustée va usté muy cargadoo."
+							likes={10}
+						/>
 					</li>
-					<li id="3">
-						<Card />
-					</li> */}
 				</ul>
 			</section>
 		);
 	}
 }
 
-
-const componentToRender = (
-	<Card
-		name="Alicia Colom"
-		date="Martes 13"
-		text="Lorem fistrum jarl a peich no puedor no puedor no te digo trigo por no
-  llamarte Rodrigor no te digo trigo por no llamarte Rodrigor ese
-  hombree qué dise usteer sexuarl. Mamaar está la cosa muy malar diodeno
-  por la gloria de mi madre diodeno me cago en tus muelas qué dise
-  usteer por la gloria de mi madre fistro se calle ustée va usté muy
-  cargadoo."
-		likes="1.000"
-	/>
-);
-ReactDOM.render(componentToRender, document.getElementById('root'));
-
-
+// const componentToRender = (
+// 	<Card
+// 		name="Alicia Colom"
+// 		date="Martes 13"
+// 		text="Lorem fistrum jarl a peich no puedor no puedor no te digo trigo por no
+//   llamarte Rodrigor no te digo trigo por no llamarte Rodrigor ese
+//   hombree qué dise usteer sexuarl. Mamaar está la cosa muy malar diodeno
+//   por la gloria de mi madre diodeno me cago en tus muelas qué dise
+//   usteer por la gloria de mi madre fistro se calle ustée va usté muy
+//   cargadoo."
+// 		likes="1.000"
+// 	/>
+// );
+// ReactDOM.render(componentToRender, document.getElementById('root'));
 
 export default MediaList;
